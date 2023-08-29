878ef9ae140690f912c2538351c155ee45e714ff
mijinB <bmj13465@gmail.com>
2023-08-29 14:34:09 +0900

    ES6 javascript로 선택적 props에 default 값 주기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Circle from "./Circle";
 
 function App() {
-  return (
-    <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" />
-    </div>
-  );
+    return (
+        <div>
+            <Circle bgColor="teal" borderColor="yellow" />
+            <Circle bgColor="tomato" text="im here" />
+        </div>
+    );
 }
 
 export default App;
