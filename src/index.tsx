6d38d63041d83be64461cb54d43d3f5b5b374b14
mijinB <bmj13465@gmail.com>
2023-08-30 17:36:37 +0900

    두 번 불러와지는 현상 해결

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,7 @@ import router from "./Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <RouterProvider router={router} />
-        </ThemeProvider>
-    </React.StrictMode>
+    <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+    </ThemeProvider>
 );
