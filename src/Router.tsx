3ce34e464b8bcb6625bbe31b0053e39f8bfeb0f1
mijinB <bmj13465@gmail.com>
2023-08-30 14:23:45 +0900

    createBrowserRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Coins from "./routes/Coins";
-import Coin from "./routes/Coin";
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
+import Coins from "./screens/Coins";
+import Coin from "./screens/Coin";
+import NotFound from "./screens/NotFound";
 
-function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Coins />} />
-                <Route path="/:coinId" element={<Coin />} />
-            </Routes>
-        </BrowserRouter>
-    );
-}
-export default Router;
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "",
+                element: <Coins />,
+            },
+            {
+                path: "/:coinId",
+                element: <Coin />,
+            },
+        ],
+        errorElement: <NotFound />,
+    },
+]);
+export default router;
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
-            <App />
+            <RouterProvider router={router} />
         </ThemeProvider>
     </React.StrictMode>
 );
