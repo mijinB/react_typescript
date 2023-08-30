a49f81f1867c478f814ee26e0e17413cc6321a7c
mijinB <bmj13465@gmail.com>
2023-08-30 16:35:28 +0900

    coin 로고img 추가

--- src/screens/Coins.tsx
+++ src/screens/Coins.tsx
@@ -23,7 +23,8 @@ const Coin = styled.li`
     background-color: white;
     color: ${(props) => props.theme.bgColor};
     a {
-        display: block;
+        display: flex;
+        align-items: center;
         padding: 20px;
         transition: color 0.2s ease-in;
     }
@@ -42,6 +43,12 @@ const Title = styled.h1`
 const Loader = styled.span`
     display: block;
     text-align: center;
+`;
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -78,7 +85,10 @@ function Coins() {
                 <CoinsList>
                     {coins.map((coin) => (
                         <Coin key={coin.id}>
-                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                            <Link to={`/${coin.id}`}>
+                                <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+                                {coin.name} &rarr;
+                            </Link>
                         </Coin>
                     ))}
                 </CoinsList>
