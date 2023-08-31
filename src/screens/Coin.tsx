94896b1748c06c2308350b8b3fd32c61aabbeae9
mijinB <bmj13465@gmail.com>
2023-08-31 11:42:51 +0900

    css순서

--- src/screens/Coin.tsx
+++ src/screens/Coin.tsx
@@ -28,19 +28,19 @@ const Loader = styled.span`
 const Overview = styled.div`
     display: flex;
     justify-content: space-between;
-    background-color: rgba(0, 0, 0, 0.5);
     padding: 10px 20px;
     border-radius: 10px;
+    background-color: rgba(0, 0, 0, 0.5);
 `;
 const OverviewItem = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     span:first-child {
+        margin-bottom: 5px;
         font-size: 10px;
         font-weight: 400;
         text-transform: uppercase;
-        margin-bottom: 5px;
     }
 `;
 const Description = styled.p`
